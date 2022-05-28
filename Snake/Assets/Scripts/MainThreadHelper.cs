ca7a126a9ce65f7b4c9c3246cc19f30dd2971cd7
Pyvel24 <goloveyan@mail.ru>
2022-05-28 19:25:25 +0300

    Create MainThreadHelper

---
+++ Snake/Assets/Scripts/MainThreadHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainThreadHelper : MonoBehaviour
+{
+    public static readonly ConcurrentQueue<Action> MainQueue = new ConcurrentQueue<Action>();
+    void Update()
+    {
+        if (!MainQueue.IsEmpty)
+        {
+            while (MainQueue.TryDequeue(out var action))
+            {
+                action?.Invoke();
+            }
+        } 
+    }
+}
