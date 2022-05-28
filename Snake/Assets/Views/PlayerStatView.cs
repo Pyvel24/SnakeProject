3db5bf7ffce4b07a561fdee89897e2aa197f82a9
Pyvel24 <goloveyan@mail.ru>
2022-05-28 19:26:24 +0300

    Create script PlayerStatView

---
+++ Snake/Assets/Views/PlayerStatView.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatView : MonoBehaviour
+{
+    public Text NameText;
+    public Text InfoText;
+}
