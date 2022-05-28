f14f4abaa1241ec6a26c87d106729c4632359b70
Pyvel24 <goloveyan@mail.ru>
2022-05-28 19:25:51 +0300

    Create script MainMenuView

---
+++ Snake/Assets/Views/MainMenuView.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuView : MonoBehaviour
+{
+    public Button StartButton;
+    public InputField ServerURL;
+}
